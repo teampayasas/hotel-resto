c00ed33b5534ab008389fdc6a7323b7359ac7e5d
Jackeline <jackelineandrea58@hotmail.com>
2022-03-10 23:02:17 +0000

    booking connected to db

--- frontend/src/components/BookForm.js
+++ frontend/src/components/BookForm.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [date, setDate] = useState('');
+    const [rooms, setRooms] = useState('');
+    const [adults, setAdults] = useState('');
+    const [kids, setKids] = useState('');
 
-    // const [name] = React.useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("This form has been submitted");
 
-    const getValues = event => {
-        event.preventDefault();
-        // console.log(name)
-        console.log(`This has been submitted`)
-        console.log(`Testing ${event.target.value}`)
-        
+        fetch('https://eu-west-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/application-0-yjsic/service/bookings/incoming_webhook/PostBooking', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({name:name, surname:surname, date:date, rooms:rooms, adults:adults, kids:kids})
+        })
+
+        setName("")
+        setSurname("")
+        setDate("")
+        setRooms("") 
+        setAdults("")
+        setKids("")
     }
 
     return (
-        <section className="form" onSubmit={getValues} >
-            <form className="submit-booking">
+        <section className="form">
+            <form onSubmit={(e) => { handleSubmit(e) }} action="/booking" method='POST' className="submit-booking">
                 <label for="name"> Name: </label> 
-                <input type="text"></input> <br/>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input> <br/>
                 <label for="surname"> Surname: </label>
-                <input type="text"></input> <br/>
+                <input type="text" value={surname} onChange={(e) => setSurname(e.target.value)}></input> <br/>
                 <label for="date"> Date: </label>
-                <input type="date"></input> <br/>
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)}></input> <br/>
                 <label for="rooms"> Rooms: </label>
-                <input type="number" min="1" step="1"></input> <br/>
+                <input type="number" min="1" step="1" value={rooms} onChange={(e) => setRooms(e.target.value)}></input> <br/>
                 <label for="adults"> Adults: </label>
-                <input type="number" min="1" step="1"></input> <br/>
+                <input type="number" min="1" step="1" value={adults} onChange={(e) => {setAdults(e.target.value)}}></input> <br/>
                 <label for="kids"> Kids: </label>
-                <input type="number" min="0" step="0"></input> <br/>
+                <input type="number" min="0" step="0" value={kids} onChange={(e) => {setKids(e.target.value)}}></input> <br/>
                 <button type="submit"> Book </button>
             </form>
         </section>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-// import App from './App';
+import './App.css';
+import App from './App';
 import Admin from './admin';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    // {/* <App /> */}
-    <Admin />,
+    <App />,
+    // <Admin />,
 
   document.getElementById('root')
 );
