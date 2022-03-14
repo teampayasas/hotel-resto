174dd730f14f93c976cbd659f6d5aad5cfa06de3
nataly <nataly.angulo530@gmail.com>
2022-03-14 01:53:41 +0000

    basic style changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,6 +3,20 @@ import React from 'react'
 
 function Footer() {
     return <footer> 
+        <section class="footerLayout">
+        <div class="fContentLayout">
+            <h2>Contact Us</h2>
+            <p>Email us at <a href="mailto:">HotelResto@amail.com</a> or call us at: 020000420000</p>
+        </div>
+        <div class="fContentLayout">
+            <h2>Location</h2>
+            <p>We are located at N1 RQ32, Halsey Rd, The Hotel Resto</p>
+        </div>
+        <div class="fContentLayout">
+            <h2>More Info</h2>
+            <p>We have customer support available to you</p>
+        </div>
+        </section>
         <p> 2022 Payasas - All Rights Reserved </p>
         </footer>
 }
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,8 +4,10 @@ import React from 'react'
 function Main() {
     return <main>
         <section className='overview' id="about-us"> 
-        <h2> Hotel Overview </h2>
+        <h2>WHO WE ARE</h2>
+        <div class="sectionLayout">
         {/* http://www.whiteskyhospitality.com/blog/wp-content/uploads/2014/11/Reach_Hotel-description-template_V2_FOR-DOWNLOAD.pdf */}
+        <img src="https://cdn.pixabay.com/photo/2016/11/17/09/28/hotel-1831072_1280.jpg"></img>
         <p> Set on the peaceful backwaters of Wymouth, this hotel welcomes guests with a beautiful outdoor pool, which overlooks the lake.
             Elegant cottages boasts a private hot tub for guests to relax in. <br />
 
@@ -18,10 +20,11 @@ function Main() {
 
             Guests can select from various spa treatments and utilize the business centre. The Resort also offers laundry services and luggage storage space. Excursions and nature treks can be arranged at the tour desk.
         </p>
+        </div>
         </section>
 
         <section className='rooms'>
-        <h2> Rooms </h2>
+        <h2>WHAT WE OFFER</h2>
         <p> <i> Deluxe Room </i> <br />
         Independent, spacious, luxuriously appointed 
         cottages of 600 soft with a private sit out 
