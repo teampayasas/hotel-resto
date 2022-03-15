5d3da11d641b736d771bed51efd30cb4edcf7e8a
Nataly Angulo <nataly.angulo530@gmail.com>
2022-03-15 17:01:00 +0000

    style changes

--- frontend/src/components/calendar.js
+++ frontend/src/components/calendar.js
@@ -1,30 +1,31 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import interactionPlugin from '@fullcalendar/interaction'
-// import "@fullcalendar/core/main.css";
-// import "@fullcalendar/daygrid/main.css";
-// let userName = "i";
-// let userId = 990;
-// let dateBooked = new Date() ;
 export default function  Cal() {
   const getData = (e)=>{
     fetch('https://eu-west-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/application-0-yjsic/service/bookings/incoming_webhook/bookingData')
     .then(res => res.json())
     .then(data => {
-        for (let index=0; index < data.length; index++){ 
-          let name = data[index].name
-          console.log(`This is the name ${name}`)
+        for (let i=0; i < data.length; i++){ 
+          let name = data[i].name
+          let surname = data[i].surname
+          let email = data[i].email
+          let bookingDate = data[i].bookingDate
+          let rooms = data[i].rooms
+          console.log(`name:${name} ${surname}, Email:${email}, Date:${bookingDate}, Room:${rooms}`)
           }
       })
     }
       return (
         <div className="Cal">
-          <FullCalendar
-            defaultView="dayGridMonth"
-            plugins={[dayGridPlugin]}
-            events={getData()}
-          />
+          <div className="calendarLayout">
+            <FullCalendar
+              defaultView="dayGridMonth"
+              plugins={[dayGridPlugin]}
+              // events={getData()}
+              events={[{url:"https://eu-west-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/application-0-yjsic/service/bookings/incoming_webhook/bookingData"}]}
+            />
+          </div>
         </div>
       );
 
