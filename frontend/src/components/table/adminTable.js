5df3199cdcf414cc7a079840d7c12c3af1b9ce54
nataly <nataly.angulo530@gmail.com>
2022-03-17 01:41:13 +0000

    adding data from database to the calendar

--- frontend/src/components/table/adminTable.js
+++ frontend/src/components/table/adminTable.js
@@ -19,55 +19,70 @@ const DataTable = () => {
     useEffect(() => {
       fetchData()
     }, [])
-  
+    // const [events, setEvents] = useState([]);
+    // console.log(users)
+  // const onEventAdded = (event) => {
+  //   setEvents([...events, event]);
+  // };
     return (
       <div>
-
-        {users.length > 0 && (
-                <table>
-                  <thead>
-                    <tr>
-                    <th>Name</th>
-                    <th>Surname</th>
-                    <th>Unique ID</th>
-                    <th>Date Booked</th>
-                    </tr>
-                  </thead>
-            {users.map(user => (
-              <tbody key= {user.id}>
-                    <tr>
-                        <td>{user.name}</td>
-                        <td>{user.surname}</td>
-                        <td>{user._id.$oid}</td>
-                        {/* <td>{user.bookingDate}</td> */}
-                        {/* <td key={users.id}>{users.name} {users.surname}- {users.bookingDate}</td>
-                        {console.log(users.id)} */}
-                    </tr>
-                    </tbody>
-            ))}
-            </table>
-            )}
-        {users.length > 0 && (
-        <div className="Cal">
-          <div className="calendarLayout">
-            <FullCalendar
-              defaultView="dayGridMonth"
-              plugins={[dayGridPlugin]}
-              // events={[{                
-              //   title: users.name,start: users.bookingDate,// id:users._id.$oid,
-              // }]}
-              events={[
-                { title: 'event 1', date: '2022-03-03' },
-                { title: 'event 2', date: '2022-03-02' }
-              ]}
-                />
-          </div>
-        </div>
+      {users.length > 0 && (
+        <table>
+                <thead>
+                  <tr>
+                  <th>Name</th>
+                  <th>Surname</th>
+                  <th>Unique ID</th>
+                  <th>Date Booked</th>
+                  </tr>
+                </thead>
+          {users.map(user => (
+            <tbody key= {user.id}>
+                  <tr>
+                      <td>{user.name}</td>
+                      <td>{user.surname}</td>
+                      <td>{user._id.$oid}</td>
+                      {/* <td>{user.bookingDate}</td> */}
+                      {/* {console.log(users)} */}
+                  </tr>
+                  </tbody>
+          ))}
+          </table>
           )}
+      {/* {users.length > 0 && ( */}
+        <div className="Cal">
+        <div className="calendarLayout">
+        {/* 
+            {/* {console.log(users)} */}
+          <FullCalendar
+            defaultView="dayGridMonth"
+            plugins={[dayGridPlugin]}
+            events={users.map(user => (
+                {title: user.name, 
+                  // date: users.bookingDate, 
+                  date: '2022-03-01', 
+                  id: user._id.$oid}
+              ))}
+
+              />
+        </div>
       </div>
+        {/* )} */}
+    </div>
       
     )
   }
     export default DataTable
     
-
+// events={[users = {
+//   title: users.name,
+//   date: '2022-03-03'
+// }]}
+// events={[{                
+//   title: users.name,start: users.bookingDate,// id:users._id.$oid,
+// }]}
+// events={[
+//   { title: 'jessie', date: '2022-03-03' }
+// ]}
+{/* <td>{user.bookingDate}</td> */}
+{/* {/* <td key={users.id}>{users.name} {users.surname}- {users.bookingDate}</td> */}
